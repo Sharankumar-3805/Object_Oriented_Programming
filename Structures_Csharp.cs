dc99cb8b76427b0d60cd65d770509f819521d50d
Sharankumar-3805 <psharan821@gmail.com>
2024-09-04 12:46:15 +0530

    oops

---
+++ Structures_Csharp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming
+{
+    struct Structures_Csharp
+    {
+        int i;
+        public void Display()
+        {
+            Console.WriteLine("Methods in structures"+i);
+        }
+        static void Main()
+        {
+            Structures_Csharp sc;
+            sc.i = 1;
+            Console.WriteLine(sc.i);
+            sc.Display();
+            Console.ReadLine();
+        }
+    }
+}
