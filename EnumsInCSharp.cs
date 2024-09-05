3f2b6366e6f55bd0931d5bd160086e58c06ebd6e
Sharankumar-3805 <psharan821@gmail.com>
2024-09-05 12:13:05 +0530

    oops

---
+++ EnumsInCSharp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming
+{
+    enum Days
+    {
+        Sunday, //0
+        Monday, //1
+        Tuesday,//2
+        Wednesday,//3
+        Thursday,//4
+        Friday,//5
+        Saturday,//6
+    }
+
+    internal class EnumsInCSharp
+    {   
+        static void Main()
+        {
+            Console.WriteLine("User-Defined Enums");
+            Console.WriteLine(Days.Monday); // 1st way calling
+            Days bday = Days.Sunday;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(bday);
+            Console.WriteLine("Built-in Enums");
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("My name is sharankumar");
+            Console.ReadLine();
+        }
+    }
+}
--- Structures_Csharp.cs
+++ Structures_Csharp.cs
@@ -9,15 +9,20 @@ namespace Object_Oriented_Programming
     struct Structures_Csharp
     {
         int i;
+        public Structures_Csharp(int i)
+        {
+            this.i = i;
+            Console.WriteLine(i);
+        }
+        
         public void Display()
         {
             Console.WriteLine("Methods in structures"+i);
         }
         static void Main()
         {
-            Structures_Csharp sc;
-            sc.i = 1;
-            Console.WriteLine(sc.i);
+            Structures_Csharp sc = new Structures_Csharp(20);
+            sc.i = 10;   
             sc.Display();
             Console.ReadLine();
         }
